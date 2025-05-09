b9a9bcc475fad45cf938e58bdf06a7e9e16ee7f8
kaushal-kj <kaushalkumbhani17@gmail.com>
2025-05-09 12:33:44 +0530

    updation

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 var app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/todo');
+mongoose.connect(process.env.MONGO_URI);
 
 const trySchema = new mongoose.Schema({
-    name: String
+  name: String,
 });
 
-const item = mongoose.model('task', trySchema);
-app.get('/',function(req, res){
-    item.find({})
-    .then(foundItems=>{
-        res.render("list",{ejes:foundItems})
+const item = mongoose.model("task", trySchema);
+app.get("/", function (req, res) {
+  item
+    .find({})
+    .then((foundItems) => {
+      res.render("list", { ejes: foundItems });
     })
-    .catch(err=>{
-        console.log(err);
-    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-app.post('/',function(req, res){
-    const itemName= req.body.ele1;
-    const todo5= new item({
-        name: itemName
-        }); 
-        todo5.save();
-        res.redirect('/');
-        });
-
-app.post("/delete", function(req, res) {
-    const checked = req.body.checkbox1;
-    
-    item.findByIdAndDelete(checked)
-      .then(() => {
-        console.log("Successfully deleted item");
-        res.redirect("/");
-      })
-      .catch(() => {
-        console.log("Error deleting item");
-      });
+app.post("/", function (req, res) {
+  const itemName = req.body.ele1;
+  const todo5 = new item({
+    name: itemName,
   });
+  todo5.save();
+  res.redirect("/");
+});
 
+app.post("/delete", function (req, res) {
+  const checked = req.body.checkbox1;
 
-app.listen("3000",function(){
-    console.log("Server started on port 3000");
+  item
+    .findByIdAndDelete(checked)
+    .then(() => {
+      console.log("Successfully deleted item");
+      res.redirect("/");
+    })
+    .catch(() => {
+      console.log("Error deleting item");
+    });
+});
+
+app.listen("3000", function () {
+  console.log("Server started on port 3000");
 });
