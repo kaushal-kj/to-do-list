308d21f776cfd7062af9e95700dcdc99c892813f
kaushal-kj <kaushalkumbhani17@gmail.com>
2025-05-09 11:52:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/todo');
+
+const trySchema = new mongoose.Schema({
+    name: String
+});
+
+const item = mongoose.model('task', trySchema);
+app.get('/',function(req, res){
+    item.find({})
+    .then(foundItems=>{
+        res.render("list",{ejes:foundItems})
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+});
+app.post('/',function(req, res){
+    const itemName= req.body.ele1;
+    const todo5= new item({
+        name: itemName
+        }); 
+        todo5.save();
+        res.redirect('/');
+        });
+
+app.post("/delete", function(req, res) {
+    const checked = req.body.checkbox1;
+    
+    item.findByIdAndDelete(checked)
+      .then(() => {
+        console.log("Successfully deleted item");
+        res.redirect("/");
+      })
+      .catch(() => {
+        console.log("Error deleting item");
+      });
+  });
+
+
+app.listen("3000",function(){
+    console.log("Server started on port 3000");
+});
